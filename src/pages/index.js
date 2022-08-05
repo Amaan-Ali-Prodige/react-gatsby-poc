358c105c8b0abf8f483ea72030bcc5fe8562ada0
Amaan-Ali-Prodige <amaan.ali@prodigedigital.com>
2022-08-05 10:49:31 +0530

    fixed redux deprecated message

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import * as React from "react"
 import { Link } from "gatsby"
 import Seo from "../components/seo"
 import Redux from "./redux"
-// import { createStore } from "redux"
 import { legacy_createStore as createStore } from "redux"
 import { Provider } from "react-redux"
 
@@ -20,23 +19,25 @@ const reducer = (state = employeeData, action) => {
 const store = createStore(reducer)
 const IndexPage = () => {
   return (
-    <Provider store={store}>
-      {/* <Router>
+    <>
+      <Provider store={store}>
+        {/* <Router>
         <Routes>
           <Route path="/page-2" exact element={<SecondPage />} />
         </Routes>
       </Router> */}
-      this is home page
-      <br />
-      <Link to="/page-2">2nd page</Link>
-      <br />
-      <Link to="/page-3">3rd page</Link>
-      <br />
-      <Link to="/number">React State Example</Link>
-      <br />
-      {/* <Link to="/redux">React Redux Example</Link> */}
-      <Redux />
-    </Provider>
+        this is home page
+        <br />
+        <Link to="/page-2">2nd page</Link>
+        <br />
+        <Link to="/page-3">3rd page</Link>
+        <br />
+        <Link to="/number">React State Example</Link>
+        <br />
+        {/* <Link to="/redux">React Redux Example</Link> */}
+        {/* <Redux /> */}
+      </Provider>
+    </>
   )
 }
 
