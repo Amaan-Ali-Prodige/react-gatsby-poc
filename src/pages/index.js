f5e6c8ed19e5e06350faf7e5bf796232833d6072
Amaan-Ali-Prodige <amaan.ali@prodigedigital.com>
2022-08-05 11:10:28 +0530

    fixed redux deprecated message again...

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,42 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import Seo from "../components/seo"
-import Redux from "./redux"
-import { legacy_createStore as createStore } from "redux"
-import { Provider } from "react-redux"
+// import Redux from "./redux"
+// import { legacy_createStore as createStore } from "redux"
+// import { Provider } from "react-redux"
 
-const employeeData = { salary: 1000 }
-const reducer = (state = employeeData, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return { salary: state.salary + 100 }
-    case "DECREMENT":
-      return { salary: state.salary - 100 }
-    default:
-      return state
-  }
-}
-const store = createStore(reducer)
+// const employeeData = { salary: 1000 }
+// const reducer = (state = employeeData, action) => {
+//   switch (action.type) {
+//     case "INCREMENT":
+//       return { salary: state.salary + 100 }
+//     case "DECREMENT":
+//       return { salary: state.salary - 100 }
+//     default:
+//       return state
+//   }
+// }
+// const store = createStore(reducer)
 const IndexPage = () => {
   return (
     <>
-      <Provider store={store}>
-        {/* <Router>
+      {/* <Provider store={store}> */}
+      {/* <Router>
         <Routes>
           <Route path="/page-2" exact element={<SecondPage />} />
         </Routes>
       </Router> */}
-        this is home page
-        <br />
-        <Link to="/page-2">2nd page</Link>
-        <br />
-        <Link to="/page-3">3rd page</Link>
-        <br />
-        <Link to="/number">React State Example</Link>
-        <br />
-        {/* <Link to="/redux">React Redux Example</Link> */}
-        {/* <Redux /> */}
-      </Provider>
+      this is home page
+      <br />
+      <Link to="/page-2">2nd page</Link>
+      <br />
+      <Link to="/page-3">3rd page</Link>
+      <br />
+      <Link to="/number">React State Example</Link>
+      <br />
+      {/* <Link to="/redux">React Redux Example</Link>
+        <Redux />
+      </Provider> */}
     </>
   )
 }
